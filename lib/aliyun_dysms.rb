073efedb94482879d447d987110e5764ea36d297
foobar <foobar@v2up.com>
2021-06-11 13:01:24 +0800

    add direct_send & global_send

--- lib/aliyun_dysms.rb
+++ lib/aliyun_dysms.rb
@@ -1,4 +1,5 @@
 require "aliyun_dysms/version"
+require "aliyunsdkcore"
 
 begin
   require "pry"
@@ -6,6 +7,76 @@ rescue LoadError
 end
 
 module AliyunDysms
-  class Error < StandardError; end
-  # Your code goes here...
+  class Configuration
+    attr_accessor :access_key_id, :access_key_secret,
+                  :region_id, :api_version,
+                  :sign_name
+
+    def initialize
+      @access_key_id = ""
+      @access_key_secret = ""
+      @sign_name = ""
+      @region_id = "cn-hangzhou"
+      @api_version = "2017-05-25"
+    end
+  end
+
+  class << self
+    attr_writer :configuration
+
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def configure
+      yield(configuration)
+    end
+
+    # TODO: 自动解析手机号判断国内国外短信
+    def send; end
+
+    def direct_send(phone_numbers, template_code, template_param, out_id = "")
+      @response = client.request(
+        action: "SendSms",
+        params: {
+          "RegionId": configuration.region_id,
+          "SignName": configuration.sign_name,
+          "PhoneNumbers": phone_numbers,
+          "TemplateCode": template_code,
+          "TemplateParam": template_param,
+          "OutId": out_id,
+        },
+        opts: { method: "POST" },
+      )
+    rescue StandardError => e
+      { Code: "BadRequest", Message: "Request failed: #<#{e.class}: #{e.message}>" }
+    end
+
+    def global_send(phone_numbers, country_code, template_code, template_param, out_id = "")
+      @response = client.request(
+        action: "SendInterSms",
+        params: {
+          "RegionId": configuration.region_id,
+          "SignName": configuration.sign_name,
+          "PhoneNumbers": phone_numbers,
+          "countryCode": country_code,
+          "TemplateCode": template_code,
+          "TemplateParam": template_param,
+          "OutId": out_id,
+        },
+        opts: { method: "POST" },
+      )
+    rescue StandardError => e
+      { Code: "BadRequest", Message: "Request failed: #<#{e.class}: #{e.message}>" }
+    end
+
+    def client
+      @client ||= RPCClient.new(
+        access_key_id: configuration.access_key_id,
+        access_key_secret: configuration.access_key_secret,
+        api_version: configuration.api_version,
+        endpoint: "https://dysmsapi.aliyuncs.com",
+      )
+    end
+  end
 end
--- spec/aliyun_dysms_spec.rb
+++ spec/aliyun_dysms_spec.rb
@@ -1,9 +1,34 @@
 RSpec.describe AliyunDysms do
-  it "has a version number" do
-    expect(AliyunDysms::VERSION).not_to be nil
-  end
+  describe "AliyunDysms" do
+    before(:all) do
+      AliyunDysms.configure do |config|
+        config.access_key_id = "test_access_key_id"
+        config.access_key_secret = "test_access_key_secret"
+        config.sign_name = "test_sign_name"
+      end
+    end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    it "has a version number" do
+      expect(AliyunDysms::VERSION).not_to be nil
+    end
+
+    it "direct_send" do
+      phone_numbers = "mobile_number"
+      template_code = "SMS_206548670"
+      template_param = { code: "123456" }
+
+      ret = AliyunDysms.direct_send(phone_numbers, template_code, template_param.to_json)
+      expect(ret["Code"]).to eql("OK")
+    end
+
+    it "global send" do
+      phone_numbers = "mobile_number"
+      country_code = "US"
+      template_code = "SMS_218172786"
+      template_param = { code: "123456" }
+
+      ret = AliyunDysms.global_send(phone_numbers, country_code, template_code, template_param.to_json)
+      expect(ret["Code"]).to eql("OK")
+    end
   end
 end
